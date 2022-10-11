2c1c3a26f6af301a2101a895a9b0c7f0e45eb315
gemtunde <gemtunde@gmail.com>
2022-10-11 03:13:09 +0100

    revenue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widget/Widget'
@@ -11,10 +13,14 @@ const Home = () => {
         <div className="homeContainer">
             <Navbar />
             <div className="widgets">
-                <Widget />
-                <Widget />
-                <Widget />
-                <Widget />
+                <Widget type='user'/>
+                <Widget type='order'/>
+                <Widget type='earning'/>
+                <Widget type='balance'/>
+            </div>
+            <div className="charts">
+                <Featured />
+                <Chart />
             </div>
         </div>
     </div>
